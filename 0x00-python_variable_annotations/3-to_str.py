958c0f2c818f88759a3214f841d041a1529ca000
jack javi <jackmtembete@gmail.com>
2024-01-12 00:00:07 +0000

    Basic annotations - to string.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Module for task 3.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Takes a float n as arguement and
+    returns the string representation of the float.
+    '''
+    return str(n)
